01f5714c75c38717bb85a8d206fc8337bba8a462
Rafael Montenegro <rafael@mesainc.com.br>
2020-02-08 21:34:12 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleClick = (event) => {
+    var x = event.pageX - event.target.offsetLeft;
+    var y = event.pageY - event.target.offsetTop;
+
+
+    const imageWidth = event.target.clientWidth
+    const imageHeight = event.target.clientHeight
+
+    const x_percent = Math.round((100 * x / imageWidth) * 100) / 100
+    const y_percent = Math.round((100 * y / imageHeight) * 100) / 100
+
+    alert(`X = ${x_percent}%\nY = ${y_percent}%`);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img onClick={handleClick} src="https://i.imgflip.com/7yhtr.jpg" alt="oh you" />
       </header>
     </div>
   );
